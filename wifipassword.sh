99b62e81aa05dee66c7881b6e2a0f37db4f3b1f1
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-01-31 00:50:55 +0530

    password cracker aircrack-ng

---
+++ wifipassword.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+aircrak(){
+    echo "Enter name of handshake file"
+    read handshake
+    echo "Enrer destination of handshake file"
+    read passwordfile
+    aircrack-ng $handshake -w $passwordfile
+}
+
+echo "Welcome to password cracker"
+echo "Press 1 to continue"
+aircrak
