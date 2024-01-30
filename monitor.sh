9fba0e7dad59b8e582fa3736b2bc809176715464
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-01-31 02:56:08 +0530

    monitor update

--- monitor.sh
+++ monitor.sh
@@ -4,9 +4,11 @@ echo "For Monitor mode press 1"
 read no
 
 monit(){
-            ifconfig wlan0 down
-            iwconfig wlan0 mode monitor
-            ifconfig wlan0 up
+            echo "Enter Device Name"
+            read n
+            ifconfig $n down
+            iwconfig $n mode monitor
+            ifconfig $n up
             iwconfig
 
 }
