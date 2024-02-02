6d4079b150002a8f0c0605070b7e262ef89b4f0a
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-02-02 23:27:57 +0530

    changes

--- Macchangerv2.sh
+++ Macchangerv2.sh
@@ -7,19 +7,30 @@ do
     Mac[i]=$n
 done
 echo ${Mac[@]}
-for j in ${Mac[@]}
-do
-    echo "How many time you want to change mac address"
-    read n
-    k=0
-    for n in k
-    do 
-        a=${Mac[0]}
-        b=${Mac[1]}
-        c=${Mac[2]}
-        d=${Mac[3]}
-        e=${Mac[4]}
-        f=${Mac[5]}
-        k=($k+1)
+echo "How many time you want to change mac address"
+read n
+k=1
+if [ $k -le $n ];
+then
+    for j in ${Mac[@]}
+    do
+        for n in k
+        do 
+            a=${Mac[0]}
+            b=${Mac[1]}
+            c=${Mac[2]}
+            d=${Mac[3]}
+            e=${Mac[4]}
+            f=${Mac[5]}
+        done
     done
-done
+    k=($k+1)
+else
+    " "
+fi
+echo $a
+echo $b
+echo $c
+echo $d
+echo $e 
+echo $f 
