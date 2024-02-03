a611f315c659933614e584eaf2eb8fd817e8821d
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-02-03 20:33:48 +0530

    updated

--- Macchangerv2.sh
+++ Macchangerv2.sh
@@ -1,40 +1,12 @@
 #!/bin/bash
-echo "Enter your Mac address bit one by one"
-Mac=()
-for i in 0 1 2 3 4 5 
-do
-    read n
-    Mac[i]=$n
-done
-echo ${Mac[@]}
-echo "How many time you want to change mac address"
+
+echo "How many times you want mac address to be changed"
 read n
-k=1
-if [ $k -le $n ];
-then
-    for j in ${Mac[@]}
-    do
-        for n in k
-        do 
-            a=${Mac[0]}
-            b=${Mac[1]}
-            c=${Mac[2]}
-            d=${Mac[3]}
-            e=${Mac[4]}
-            f=${Mac[5]}
-        done
-    done
-    k=($k+1)
-else
-    " "
-fi
-echo $a
-echo $b
-echo $c
-echo $d
-echo $e 
-echo $f 
-mac=$a:$b:$c:$d:$e:$f
-ifconfig wlo1 down
-ifconfig wlo1 hw ether $mac
-ifconfig up
+echo "Enter device name"
+read device
+ifconfig $device down
+for i in $n 
+do
+    macchanger -e $device
+done
+ifconfig $device up
