f18326f6c140ab1a1d1675dd89b1d1724ba66d81
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-02-05 10:07:42 +0530

    change 1

---
+++ scanv2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Enter Traget ip"
+read ip
+echo "Enter service you want to run"
+echo "1.Basic port scan"
+echo "2.Service Scan"
+echo "3.Version scan"
+echo "4.scripts scan"
+read scan
