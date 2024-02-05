5b0fa5e7e4c6693ac53e7d3e3e2edbafd56ee0ca
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-02-06 01:12:03 +0530

    hashcat

--- passwordcracker.sh
+++ passwordcracker.sh
@@ -8,3 +8,24 @@ read choice
 echo "Do you have hash of the file "
 echo "1.Yes"
 echo "2.NO"
+echo "900 | MD4
+0 | MD5
+ 100 | SHA1
+1300 | SHA2-224
+1400 | SHA2-256
+10800 | SHA2-384
+1700 | SHA2-512
+17300 | SHA3-224
+17400 | SHA3-256
+17500 | SHA3-384
+17600 | SHA3-512"
+echo "Enter hash type "
+read hashtype
+echo "Enter hash file location in txt format"
+read hashdestination
+echo "Enter wordlist location"
+read wordlist
+if [ $choice -eq 1 ];
+then
+    hashcat -a 0 -m $hashtype $hashdestination $wordlist
+fi
