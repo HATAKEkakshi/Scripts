f66e5f71091631e6b3d1e4fdd1372d2ca88aaa0e
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-02-05 10:12:22 +0530

    updated

--- scanv2.sh
+++ scanv2.sh
@@ -8,3 +8,16 @@ echo "2.Service Scan"
 echo "3.Version scan"
 echo "4.scripts scan"
 read scan
+if [ $scan -eq 1 ];
+then
+    nmap $ip
+elif [ $scan -eq 2 ];
+then
+    nmap -sC 
+elif [ $scan -eq 3 ];
+then
+    nmap -sV
+elif [$scan -eq 4 ];
+then
+    nmap -p- -sC -sV $ip 
+fi
