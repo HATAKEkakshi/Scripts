15bdea805922766a57a31243f1e1b4bd4e4105db
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-02-07 01:26:02 +0530

    linux cheat

---
+++ linuxcheat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "<---------------------WELCOME TO LINUX CHEAT ------------------>"
+echo "Select from the following commands"
+echo "1.Copy"
+echo "2.Delete"
+echo "3.create"
+echo "4.move"
+read choice
+if []
