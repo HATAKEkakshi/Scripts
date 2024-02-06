4107d218734b74ae881d7e0be1a86e934ea7c173
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-02-07 00:08:18 +0530

    single copy function

--- copy.sh
+++ copy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-
+echo "Select Do you have multiple files"
+echo "Do you have multiple files"
+echo "1.Yes"
+echo "2.No"
+read choice
+if [ $choice -eq 1 ];
+then
+else
+    echo "Enter location of file you to copy from"
+    read location
+    echo "Enter location of file you want to copy it"
+    read data
+    cp $location $data
+f
