751b03016c28a1ff8ccf3a626fe2d6a36c35d442
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-02-07 01:33:23 +0530

    linuxcheat

--- linuxcheat.sh
+++ linuxcheat.sh
@@ -6,5 +6,20 @@ echo "1.Copy"
 echo "2.Delete"
 echo "3.create"
 echo "4.move"
+echo "<-------------------------------------------------------------->"
 read choice
-if []
+if [ $choice -eq 1 ];
+then
+    ./copy.sh
+elif [ $choice -eq 2 ];
+then
+    ./remove.sh
+elif [ $choice -eq 3 ];
+then
+    echo "Enter folder name you want to create"
+    read folder
+    mkdir $folder
+elif [ $choice -eq 4 ];
+then
+    ./move.sh
+fi
