e9d575aae1129ada29ff884d3158aa8ea23e1ba0
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-02-07 00:17:35 +0530

    multiple file problem solved

--- copy.sh
+++ copy.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+loopcopy(){
+    echo "Do you multiple files from same folder"
+    echo "1.Yes"
+    echo "2.No"
+    read cho
+    if [ $cho -eq 1 ];
+    then
+        echo "Enter destination of fixed folder"
+        read destination
+        echo "Enter name or destination of folder or file in which data has to be copied to"
+        read master
+        echo "Enter how many multiple file of the same folder you have"
+        read n
+        for ((i=1;i<=n;i++))
+        do
+            echo "Enter file name"
+            read filename
+            cp $destination/$filename $master
+        done  
 
+
+}
 echo "Select Do you have multiple files"
 echo "Do you have multiple files"
 echo "1.Yes"
@@ -7,6 +28,7 @@ echo "2.No"
 read choice
 if [ $choice -eq 1 ];
 then
+
 else
     echo "Enter location of file you to copy from"
     read location
