2a4addf31750ede8d4969042ffb6b347bc8f79fb
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-02-11 20:14:35 +0530

    added coomom credtinaials

--- passwordcracker.sh
+++ passwordcracker.sh
@@ -90,6 +90,122 @@ passwordfile(){
             then
                 wordlist=$direct/SecLists/Passwords/Honeypot-Captures/wordpress-attacks-july2014.txt
             fi
+        elif [ $cracked -eq 5 ];
+        then
+            echo "<------------------SELECT FROM THE FOLLOWING---------------------------->"
+            echo "1.Password List 1"
+            echo "2.Password List 2"
+            echo "3.Password List 3"
+            echo "<---------------------------------------------------------------------->"
+            read we
+            if [ $we -eq 1 ];
+            then
+                wordlist=$direct/SecLists/Passwords/WiFi-WPA/probable-v2-wpa-top447.txt
+            elif [ $we -eq 2 ];
+            then
+                wordlist=$direct/SecLists/Passwords/WiFi-WPA/probable-v2-wpa-top62.txt
+            elif [ $we -eq 3 ];
+            then
+                wordlist=$direct/SecLists/Passwords/WiFi-WPA/probable-v2-wpa-top4800.txt
+            fi
+        elif [ $cracked -eq 6 ];
+        then
+            echo "<-------------------- SELECT FROM THE FOLLOWING LIST-------------------->"
+            echo "1.Password List  01"
+            echo "2.Password List  02"
+            echo "3.Password List  03"
+            echo "4.Password List  04"
+            echo "5.Password List  05"
+            echo "6.Password List  06"
+            echo "7.Password List  07"
+            echo "8.Password List  08"
+            echo "9.Password List  09"
+            echo "10.Password List 10"
+            echo "11.Password List 11"
+            echo "12.Password List 12"
+            echo "13.Password List 13"
+            echo "14.Password List 14"
+            echo "15.Password List 15"
+            echo "16.Password List 16"
+            echo "17.Password List 17"
+            echo "18.Password List 18"
+            echo "19.Password List 19"
+            echo "20.Password List 20"
+            echo "21.Password List 21"
+            echo "<---------------------------------------------------------------------------------------->"
+            read kl
+            if [ $kl -eq 1 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/10k-most-common.txt 
+            elif [ $kl -eq 2 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/10-million-password-list-top-100.txt 
+            elif [ $kl -eq 3 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/10-million-password-list-top-500.txt
+            elif [ $kl -eq 4 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/10-million-password-list-top-1000.txt
+            elif [ $kl -eq 5 ];
+            then 
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/10-million-password-list-top-10000.txt 
+            elif [ $kl -eq 6 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/10-million-password-list-top-100000.txt 
+            elif [ $kl -eq 7 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
+            elif [ $kl -eq 8 ];
+            then 
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/500-worst-passwords.txt
+            elif [ $kl -eq 9 ];
+            then 
+                 wordlist=$direct/SecLists/Passwords/Common-Credentials/1900-2020.txt
+            elif [ $kl -eq 10 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/best15.txt 
+            elif [ $kl -eq 11 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/best10.txt 
+            elif [ $kl -eq 12 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/best1050.txt 
+            elif [ $kl -eq 13 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/common-passwords-win.txt 
+            elif [ $kl -eq 14 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/four-digit-pin-codes-sorted-by-frequency-withcount.csv 
+            elif [ $kl -eq 15 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/medical-devices.txt 
+            elif [ $kl -eq 16 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/SplashData-2014.txt
+            elif [ $kl -eq 17 ];
+            then 
+                wordlist=$directSecLists/Passwords/Common-Credentials/SplashData-2015-1.txt 
+            elif [ $kl -eq 18 ];
+            then
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/SplashData-2015-2.txt
+            elif [ $kl -eq 19 ];
+            then 
+                wordlist=$direct/SecLists/Passwords/Common-Credentials/top-20-common-SSH-passwords.txt 
+            elif [ $kl -eq 20 ];
+            then
+            wordlist=$direct/SecLists/Passwords/Common-Credentials/top-passwords-shortlist.txt 
+            elif [ $kl -eq 21 ];
+            then
+            wordlist=$direct/SecLists/Passwords/Common-Credentials/worst-passwords-2017-top100-slashdata.txt 
+            fi
+        elif [ $cracked -eq 7 ];
+        then
+        elif [ $cracked -eq 8 ];
+        then
+        elif [ $cracked -eq 9 ];
+        then
+        elif [ $cracked -eq 10 ];
+        then
         fi
     else
         echo "Enter the destination of the file of wordlist"
