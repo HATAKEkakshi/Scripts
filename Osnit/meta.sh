8d2b5bee7f711375322a4349ba7a73ff69a71ef7
root <root@localhost.localdomain>
2024-04-09 13:49:11 -0400

    META.SH

--- Osnit/meta.sh
+++ Osnit/meta.sh
@@ -21,16 +21,28 @@ echo "2.No"
 read choice
 if [ $choice -eq 1 ];
 then
-    echo "coming_soon"
+    echo "Enter target_username"
+    read target_username
+    python3 main.py $target_username
 elif [ $choice -eq 2 ];
 then
     break
 fi
-echo "Enter instagram username"
-echo "Enter how many target photo you have"
-read n
-for (( i=1;i<=n;i++));
-do
-    read target_$i
-    exiv2 pr target_$i
-done
+echo "Do you have Target Photos"
+echo "1.Yes"
+echo "2.No"
+read photo
+if [ $photo -eq 1 ];
+then
+    echo "Enter how many target photo you have"
+    read n
+    for (( i=1;i<=n;i++));
+    do
+        read target_$i
+        exiv2 pr target_$i
+    done
+elif [ $photo -eq 2 ];
+then
+    exit
+fi
+
